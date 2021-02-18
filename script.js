bb9a1c9961f4d05ddad57987bf7a432a6700e9ab
marymotlagh <mary.motlagh@gmail.com>
2021-02-18 14:43:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = document.getElementById("principal").value;
+    if (principal <= 0) {
+        document.getElementById("divError").className = "show errorColor";
+        document.getElementById("result").className = "fieldsRow label hide";
+    }
+    else {
+        document.getElementById("divError").className = "hide errorColor";
+        var todayDate = new Date();
+        var todayYear = todayDate.getFullYear();
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var sum = (principal * rate / 100) * years;
+        document.getElementById("principalVal").innerHTML = principal;
+        document.getElementById("percentageVal").innerHTML = rate;
+        document.getElementById("sum").innerHTML = sum;
+        document.getElementById("year").innerHTML = parseInt(todayYear) + parseInt(years);
+        document.getElementById("result").className = "fieldsRow label show";
+    }
 }
-        
+function updateTextInput(val) {
+    document.getElementById('percentage').innerHTML = val + "%";
+}
+
+function setValue() {
+    updateTextInput("10.50");
+}
